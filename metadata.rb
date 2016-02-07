0b265fd566cf4350e73a6ed85c89833b18b9cd6c
ricardolupo <rlupo@getchef.com>
2016-02-06 22:21:10 -0500

    bump metadata to reflect minor update

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email ''
 license          'Apache 2.0'
 description      'Installs, configures and runs IBM Installation Manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.1'
+version          '0.5.0'
 
 supports "linux"
